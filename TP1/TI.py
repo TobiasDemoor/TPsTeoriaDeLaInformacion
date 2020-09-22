c9754ed89372a5615ef2ab1b237f8201a51c1f4f
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-09-22 12:01:08 -0300

    entropiaBin y checkeo de probabilidad nula

--- TP1/TI.py
+++ TP1/TI.py
@@ -5,8 +5,16 @@ from SIM import Dist, DistExp, DistExpFactory, DistSimulada, DistTeoricaDiscreta
 
 
 def binaria(w: float) -> DistExp:
-    """Metodo factory para una distribución binaria con P(0) = w"""
+    """Método factory para una distribución binaria con P(0) = w"""
+
     return DistExp([0,1], [w ,1])
+
+def entropiaBin(w: float) -> float:
+    """Método para calcular la entropía de una distribución binaria con P(0) = w"""
+
+    if (w == 0) or (w == 1): return 0
+    return w*-log2(w)+(1-w)*-log2(1-w)
+
 
 def secuencia(self: Dist, n: int) -> list:
     """Genera una secuencia de n valores al azar según la distribución"""
@@ -19,14 +27,15 @@ def secuencia(self: Dist, n: int) -> list:
 
 def cantInformacion(self: Dist, dato) -> float:
     """Calcula la cantidad de información en bits de un dato dado"""
-
-    return -log2(self.prob(dato))
+    p = self.prob(dato)
+    if p == 0: return 0
+    return -log2(p)
 
 
 def entropia(self: Dist) -> float:
     ent = 0
     for i in self.valores:
-        ent += -self.prob(i)*log2(self.prob(i))
+        ent += self.prob(i)*self.cantInformacion(i)
     return ent
 
 
