3b7f855cbbd17f142832256d1ce8b232e00dd54e
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-09-29 09:46:28 -0300

    prueba y validacion

--- TP1/main.py
+++ TP1/main.py
@@ -3,7 +3,23 @@ import math
 import pandas
 from fuentesDeInfo import Dist, DistExp, DistExpFactory, DistSimulada, DistTeoricaDiscreta
 
-d = DistExpFactory.fromMuestra([0,1,2,3])
-# print(d.cantInformacion(1))
-print(d.entropia())
-print(d.reporte())
+# Anexo 1
+d1 = DistExpFactory.fromProbAbs(
+    ['a', 'b', 'c', 'd'],
+    [0.605, 0.108, 0.174, 0.113]
+)
+d2 = DistExpFactory.fromProbAbs(
+    ['a', 'b', 'c', 'd', 'e'],
+    [0.0446, 0.211, 0.338, 0.331, 0.0754]
+)
+d3 = DistExpFactory.fromProbAbs(
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+    [0.142, 0.0519, 0.00323, 0.245, 0.0587, 0.387, 0.11217]
+)
+
+print(f"H(S1) = {d1.entropia()}")
+print(d1.reporte())
+print(f"H(S2) = {d2.entropia()}")
+print(d2.reporte())
+print(f"H(S3) = {d3.entropia()}")
+print(d3.reporte())
