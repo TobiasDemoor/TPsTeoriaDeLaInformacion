f3f06e631aab31058dc3aefaedf399dcd0d02ec5
Leopoldo Lening Celaya <57147656+pololeningcelaya@users.noreply.github.com>
2020-10-13 17:05:16 -0300

    Movimiento celestial

--- TP1/fuentesDeInfo.py
+++ TP1/fuentesDeInfo.py
@@ -57,44 +57,6 @@ def extensionDeFuente(fuente: Dist, orden:int):
         
     return DistExpFactory.fromProbAbs(valoresAnt, probAnt)
 
-def esCodBloque(aCodigo: list, codigoBloque: list) -> bool:
-    bloqueaux = codigoBloque[:]
-    aaux = aCodigo[:]
-    longmax = len(max(bloqueaux, key=len))
-    it = 0
-    while len(bloqueaux) > 0 and it < longmax:
-        for i in aaux:
-            while i in bloqueaux:
-                bloqueaux.remove(i)
-        lista = []
-        for i in aaux:
-            for j in aCodigo:
-                lista.append(i + j)
-        aaux = lista
-        it += 1
-    return len(bloqueaux) == 0
-
-def esNoSingular(codigoBloque: list) -> bool:
-    return len(set(codigoBloque)) == len(codigoBloque)
-        
-def esInstantaneo(codigoBloque: list) -> bool:
-    for i, x in enumerate(codigoBloque):
-        for j, y in enumerate(codigoBloque):
-            if y.find(x) == 0 and i != j:
-                return False
-    return True
-    # version fea
-    # i, cumple = 0, True
-    # largo = len(codigoBloque)
-    # while i < largo and cumple:
-    #     x, j = codigoBloque[i], 0
-    #     while j < largo and cumple:
-    #         y = codigoBloque[j]
-    #         if y.find(x) == 0 and i !=j:
-    #             cumple = False
-    #         j += 1
-    #     x += 1
-    # return cumple
 
 #Se añaden las funciones a la clase Dist dinámicamente
 Dist.secuencia = secuencia
