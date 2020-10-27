0d5b1efc150af2242d2ea6666704bfa348fee3ce
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-10-27 18:47:50 -0300

    shannonfano y huffman

--- General/codificacion.py
+++ General/codificacion.py
@@ -42,12 +42,12 @@ class CodigoBloque:
         """
 
         entropia, df = self.fuente.reporte()
-        df.insert(1, "Codigo", list(map(lambda x: self.codigos[x], self.fuente.ids)))
+        df.insert(1, "Codigo", list(
+            map(lambda x: self.codigos[x], self.fuente.ids)))
         longM = f"\nLongitud media: {self.longMedia()}"
         comp = f"\nEs compacto: {esCompacto(self)}"
         kraft = f"\nCumple Kraft: {cumpleKraft(self, 2)}"
         return entropia, df, longM, comp, kraft
-
 
 
 class CodigoBloqueFactory:
@@ -59,7 +59,7 @@ class CodigoBloqueFactory:
         """
 
         fuente = FuenteDeInfoFactory.equiprobable(ids)
-        dcodigos = {i:j for i, j in zip(ids, codigos)}
+        dcodigos = {i: j for i, j in zip(ids, codigos)}
         return CodigoBloque(fuente, dcodigos)
 
     @staticmethod
@@ -70,7 +70,7 @@ class CodigoBloqueFactory:
         """
 
         fuente = FuenteDeInfoFactory.crear(ids, probs)
-        dcodigos = {i:j for i, j in zip(ids, codigos)}
+        dcodigos = {i: j for i, j in zip(ids, codigos)}
         return CodigoBloque(fuente, dcodigos)
 
     @staticmethod
@@ -80,18 +80,19 @@ class CodigoBloqueFactory:
             siendo el código generado binario e instantaneo
         """
 
-        dic = {i : ceil(fuente.cantInformacion(i)) for i in fuente.ids}
+        dic = {i: ceil(fuente.cantInformacion(i)) for i in fuente.ids}
         sig, ant = "", 0
-        for k, v in sorted(dic.items(), key = lambda x: x[1]):
-            if v == 0: v = 1
+        for k, v in sorted(dic.items(), key=lambda x: x[1]):
+            if v == 0:
+                v = 1
             if v != ant:
                 sig += "0" * (v - ant)
                 ant = v
             dic[k] = sig
-            # bin() devuelve "0bXXXXX" por lo que omitimos los dos primeros valores 
+            # bin() devuelve "0bXXXXX" por lo que omitimos los dos primeros valores
             sig = bin(int(sig, 2)+1)[2:].zfill(v)
         return CodigoBloque(fuente, dic)
-    
+
     @staticmethod
     def extensionDeFuente(codigoBloque: CodigoBloque, orden: int) -> CodigoBloque:
         """
@@ -117,6 +118,70 @@ class CodigoBloqueFactory:
 
         return CodigoBloqueFactory.crear(idAnt, codAnt, probAnt)
 
+    @staticmethod
+    def __huffmanRec(probs: dict) -> dict:
+        orden = list(probs.keys())
+        l = len(orden)
+        if l > 2:
+            orden.sort(reverse=True, key=lambda x: probs[x])
+            combinados = (orden[-2], orden[-1])
+            # modifico el dict de probabilidad para pasarlo al paso siguiente
+            probs[combinados[0]] = probs[combinados[0]] + probs[combinados[1]]
+            del probs[combinados[1]]
+            res = CodigoBloqueFactory.__huffmanRec(probs)
+            codComb = res[combinados[0]]
+            res[combinados[0]] = codComb + "0"
+            res[combinados[1]] = codComb + "1"
+        else:
+            if l == 2:
+                res = {orden[0]: "1", orden[1]: "0"}
+            else:
+                res = {orden[0]: "" }
+        return res
+
+    @staticmethod
+    def huffman(fuente: FuenteDeInfo) -> CodigoBloque:
+        # clono el diccionario para no destruirlo
+        codigo = CodigoBloqueFactory.__huffmanRec(fuente.probs.copy())
+        return CodigoBloque(fuente, codigo)
+
+    @staticmethod
+    def __shannonFanoRec(probs: dict, tope: float):
+        orden = list(probs.keys())
+        orden.sort(reverse=True, key=lambda x: probs[x])
+        l = len(orden)
+        if l > 2:
+            t, suma = 0, 0
+            dif, minimo = 1, 2
+            while dif < minimo:
+                suma += probs[orden[t]]
+                t += 1
+                minimo, dif = dif, abs(tope - 2*suma)
+
+            menoresDict = {orden[k]: probs[orden[k]] for k in range(t)}
+            mayoresDict = {orden[k]: probs[orden[k]] for k in range(t, l)}
+            res = {}
+            resAux = CodigoBloqueFactory.__shannonFanoRec(menoresDict, suma)
+            for k, v in list(resAux.items()):
+                res[k] = "1" + v
+
+            resAux = CodigoBloqueFactory.__shannonFanoRec(mayoresDict, tope - suma)
+            for k, v in list(resAux.items()):
+                res[k] = "0" + v
+        else:
+            if l == 2:
+                res = {orden[0]: "1", orden[1]: "0"}
+            else:
+                res = {orden[0]: "" }
+        return res
+
+
+    @staticmethod
+    def shannonFano(fuente: FuenteDeInfo) -> CodigoBloque:
+        codigo = CodigoBloqueFactory.__shannonFanoRec(fuente.probs, 1)
+        return CodigoBloque(fuente, codigo)
+
+
 
 def cumpleKraft(codigo: CodigoBloque, r: int) -> bool:
     suma = 0
@@ -126,9 +191,9 @@ def cumpleKraft(codigo: CodigoBloque, r: int) -> bool:
             return False
     return True
 
-    
+
 def esCompacto(codigo: CodigoBloque) -> bool:
-    for i  in codigo.fuente.ids:
+    for i in codigo.fuente.ids:
         if len(codigo.codigo(i)) > ceil(codigo.fuente.cantInformacion(i)):
             return False
     return True
@@ -163,3 +228,28 @@ def esInstantaneo(codigoBloque: CodigoBloque) -> bool:
                 return False
     return True
 
+
+# fuente = FuenteDeInfo(["s1", "s2", "s3", "s4"], {
+#     "s1": 0.6,
+#     "s2": 0.2,
+#     "s3": 0.1,
+#     "s4": 0.1
+# })
+
+# fuente = FuenteDeInfoFactory.crear(
+#     ["S1", "S2", "S3", "S4"], [4/10, 1/10, 3/10, 2/10])
+
+
+# fuente = FuenteDeInfo(["s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8"], {
+#     "s1": 0.40,
+#     "s2": 0.20,
+#     "s3": 0.15,
+#     "s4": 0.10,
+#     "s5": 0.06,
+#     "s6": 0.04,
+#     "s7": 0.03,
+#     "s8": 0.02
+# })
+
+# # print(*CodigoBloqueFactory.huffman(fuente).reporte())
+# print(*CodigoBloqueFactory.shannonFano(fuente).reporte())
