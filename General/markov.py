41b63b474fab263d74c67123c4791f82d04c6d3a
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-11-11 19:18:02 -0300

    upa

--- General/markov.py
+++ General/markov.py
@@ -42,9 +42,22 @@ class FuenteDeMarkov:
             return self.__grafo
         except AttributeError:
             dot = Digraph(node_attr={'shape': 'circle'})
-            for i, valO in enumerate(self.ids):
-                for j, valD in enumerate(self.ids):
+            for j, valO in enumerate(self.ids):
+                for i, valD in enumerate(self.ids):
                     if self.mat[i][j] != 0:
-                        dot.edge(valO, valD, str(round(self.mat[i][j], 5)))
+                        dot.edge(valO, valD, str(round(self.mat[j][i], 5)))
             self.__grafo = dot
             return self.__grafo
+
+
+markov2 = FuenteDeMarkov(
+    ["A", "B", "C", "D", "E"],
+    [
+        [0.163, 0.00693, 0.694, 0.0406, 0.463],
+        [0.259, 0.0655, 0.0817, 0.329, 0.099],
+        [0.251, 0.514, 0.0517, 0.179, 0.228],
+        [0.222, 0.361, 0.00799, 0.0948, 0.1],
+        [0.105, 0.05257, 0.16461, 0.3566, 0.11]
+    ])
+
+markov2.grafo.render("markov", format="png")
