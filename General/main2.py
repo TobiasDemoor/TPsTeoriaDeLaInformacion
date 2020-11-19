5c1f3c4a327ff7be1f181ff3f99edbb8d6619849
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2020-11-19 16:53:14 -0300

    formato

--- General/main2.py
+++ General/main2.py
@@ -1,5 +1,6 @@
 #################### Primera Parte ####################
 from codificacion import CodigoBloque, CodigoBloqueFactory, codificaRLC, decoRLC
+
 # Fuente 1
 arch = open("mdp-espanol.txt", "r", encoding="utf-8")
 
@@ -8,6 +9,7 @@ arch = open("mdp-portugues.txt", "r", encoding="utf-8")
 
 #################### Segunda Parte ####################
 from canales import Canal, CanalFactory
+
 # Canal 1
 simbIn = ["A1", "A2"]
 simbOut = ["B1", "B2", "B3", "B4", "B5"]
@@ -16,7 +18,7 @@ mat = [
     [0.2, 0.2, 0.2, 0.2, 0.2],
     [0.9, 0.1, 0.0, 0.0, 0.0]
 ]
-canal = CanalFactory.fromMat(simbIn, simbOut, mat, probIn)
+canal1 = CanalFactory.fromMat(simbIn, simbOut, mat, probIn)
 
 # Canal 2
 simbIn = ["A1", "A2", "A3", "A4", "A5"]
@@ -29,7 +31,7 @@ mat = [
     [0.5, 0.4, 0.1, 0.0, 0.0],
     [0.0, 0.0, 0.0, 1.0, 0.0]
 ]
-canal = CanalFactory.fromMat(simbIn, simbOut, mat, probIn)
+canal2 = CanalFactory.fromMat(simbIn, simbOut, mat, probIn)
 
 # Canal 3
 simbIn = ["A1", "A2", "A3", "A4"]
@@ -41,4 +43,4 @@ mat = [
     [0.0, 0.5, 0.5],
     [1.0, 0.0, 0.0]
 ]
-canal = CanalFactory.fromMat(simbIn, simbOut, mat, probIn)
+canal3 = CanalFactory.fromMat(simbIn, simbOut, mat, probIn)
